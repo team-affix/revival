12cfc7e8ba6d500d48347cdf2cbb3f9a883f351a
teamaffixmb-jake <teamaffixmb.jake@gmail.com>
2025-03-26 18:55:35 -0700

    Added makefile and updated lambda code

--- lambda/agda-typecheck/ex.agda
+++ lambda/agda-typecheck/ex.agda
@@ -1,1 +1,6 @@
-123
+module ex where
+
+data ⊥ : Set where
+
+data not (X : Set) : Set where
+    not-intro : (X -> ⊥) -> not X
--- lambda/agda-typecheck/lambda_function.py
+++ lambda/agda-typecheck/lambda_function.py
@@ -7,19 +7,24 @@ s3Client = boto3.client('s3')
 
 def handler(event, context):
     # test123 = subprocess.run(['agda', '--version'], capture_output=True, text=True)
-    test1234 = subprocess.run(['agda', './ex.agda'], capture_output=True, text=True)
+    # test1234 = subprocess.run(['agda', './ex.agda'], capture_output=True, text=True)
     # Get the file path to typecheck in s3 bucket
     ld = os.listdir('/opt/agda')
-    # s3_file_path = event["obj_key"]
-    # print("agda_file_path   : " + s3_file_path)
+    s3_file_path = event["obj_key"]
+    print("agda_file_path   : " + s3_file_path)
 
-    # # Our bucket name
-    # BUCKET_NAME = "revival-logic-platform"
+    s3_file_name = os.path.basename(s3_file_path)
 
-    # # Get the object
-    # LOCAL_FILE_NAME = '/tmp/tmp.agda'
-    # s3Client.download_file(BUCKET_NAME, s3_file_path, LOCAL_FILE_NAME)
+    # Our bucket name
+    BUCKET_NAME = "revival-logic-platform"
 
-    # result = subprocess.run(["agda", LOCAL_FILE_NAME], capture_output=True, text=True)
+    # Get the object
+    LOCAL_FILE_NAME = os.path.join("/tmp", s3_file_name)
+    s3Client.download_file(BUCKET_NAME, s3_file_path, LOCAL_FILE_NAME)
 
-    return str(ld) + str(event) + str(test1234)
+    # read file
+    fcontent = open(LOCAL_FILE_NAME, 'r').read()
+
+    result = subprocess.run(["agda", "-i/tmp", LOCAL_FILE_NAME], capture_output=True, text=True)
+
+    return str(event) + "\n" + str(fcontent) + "\n" + str(result)
