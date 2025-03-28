3a08922fd9e177f1a71db2dcc8d4701c21511236
teamaffixmb-jake <teamaffixmb.jake@gmail.com>
2025-03-27 23:31:07 -0700

    Added vue 3 with typescript frontent

---
+++ frontend/revival/src/App.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="app">
+        <p>{{ field }}</p>
+        <button @click="changeField(444)">change field</button>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name: 'App',
+    components: {},
+    data() {
+        return {
+            field: "lll",
+        };
+    },
+    methods: {
+        changeField(field: string): void {
+            this.field = field
+        }
+    },
+});
+
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
