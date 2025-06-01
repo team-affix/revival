6c4512feadf6393d5a28a4f76b5678a6f9d5771a
user <user@desktop.lan>
2025-05-31 21:27:49 -0700

    Made it so package pulling lambda function pulls a package specified by the input json

--- lambda/pull/function/index.js
+++ lambda/pull/function/index.js
@@ -4,17 +4,18 @@ const s3 = new AWS.S3();
 const bucket = process.env.BUCKET_NAME;
 
 exports.handler = async (event) => {
-  const key = 'temp.txt';
-    // const key = event.package_name + '/' + event.package_version;
+//   const key = 'temp.txt';
+    const key = event.package_name + '/' + event.package_version + '.zip';
 
   try {
     const data = await s3.getObject({ Bucket: bucket, Key: key }).promise();
-    const content = data.Body.toString('utf-8');
+    const content = data.Body.toString('base64');
 
     return {
-      statusCode: 200,
-      body: JSON.stringify({ content }),
-      bucket_name: bucket,
+        statusCode: 200,
+        isBase64Encoded: true,
+        body: content,
+        bucket_name: bucket,
     };
   } catch (err) {
     console.error(err);
--- lambda/pull/invoke.sh
+++ lambda/pull/invoke.sh
@@ -1,3 +1,8 @@
-aws lambda invoke --function-name lpk-revival-package-pull --payload file://payload.json --cli-binary-format raw-in-base64-out out.txt
-cat out.txt
-rm out.txt
+rm -rf ./absurdity
+
+aws lambda invoke --function-name lpk-revival-package-pull --payload file://payload.json --cli-binary-format raw-in-base64-out out.json
+
+jq -r '.body' out.json | base64 --decode > response.zip
+rm out.json
+unzip response.zip
+rm response.zip
