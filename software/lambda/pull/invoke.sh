62811d871538b210cae93eb70b7bba29469c9308
teamaffixmb-jake <teamaffixmb.jake@gmail.com>
2025-06-17 17:07:17 -0700

    Added invocation thru api-gateway to invoke script

--- software/lambda/pull/invoke.sh
+++ software/lambda/pull/invoke.sh
@@ -6,25 +6,41 @@
 set -e
 
 ENVIRONMENT=$1
+ENTRYPOINT=$2
 
-if [ -z "$ENVIRONMENT" ]; then
-    echo "Usage: $0 <environment>"
-    echo "Example: $0 dev"
-    echo "Example: $0 prod"
+if [ -z "$ENVIRONMENT" ] || [ -z "$ENTRYPOINT" ]; then
+    echo "Usage: $0 <environment> <entrypoint>"
+    echo "Example: $0 dev cli"
+    echo "Example: $0 prod api-gateway"
     exit 1
 fi
 
 rm -rf ./absurdity
 
-# Get the lambda function name
-cd ../../../infrastructure/terragrunt/$ENVIRONMENT/lambda
-lambda_function_name=$(terragrunt output -raw lambda_function_name)
-cd ../../../../software/lambda/pull
 
-# Invoke the lambda function
-aws lambda invoke --function-name "$lambda_function_name" --payload file://payload.json --cli-binary-format raw-in-base64-out out.json
+if [ "$ENTRYPOINT" == "cli" ]; then
+    # Get the lambda function name
+    cd ../../../infrastructure/terragrunt/$ENVIRONMENT/lambda
+    lambda_function_name=$(terragrunt output -raw lambda_function_name)
+    cd ../../../../software/lambda/pull
+    # Invoke the lambda function
+    aws lambda invoke --function-name "$lambda_function_name" --payload file://payload.json --cli-binary-format raw-in-base64-out out.json
+    jq -r '.body' out.json | base64 --decode > response.zip
 
-jq -r '.body' out.json | base64 --decode > response.zip
+elif [ "$ENTRYPOINT" == "api-gateway" ]; then
+    # Get the api gateway url
+    api_gateway_url=$(cd ../../../infrastructure/terragrunt/$ENVIRONMENT/api-gateway && terragrunt output -raw api_gateway_url)
+
+    echo "🚀 Invoking lambda function thru api gateway..."
+    echo "API Gateway URL: $api_gateway_url"
+
+    # Invoke the lambda function thru curl
+    curl -X POST $api_gateway_url \
+    -H "Content-Type: application/json" \
+    -d @payload.json | base64 --decode > response.zip
+
+fi
+
 # rm out.json
 unzip response.zip
 rm response.zip
