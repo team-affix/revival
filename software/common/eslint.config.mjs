ca58b33a335af82d401ce48fd1544edb4e4637e8
teamaffixmb-jake <teamaffixmb.jake@gmail.com>
2025-07-07 00:37:44 -0700

    Added linting for floating promises

--- software/common/eslint.config.mjs
+++ software/common/eslint.config.mjs
@@ -3,4 +3,15 @@
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
 
-export default tseslint.config(eslint.configs.recommended, tseslint.configs.strict, tseslint.configs.stylistic);
+export default tseslint.config(eslint.configs.recommended, tseslint.configs.strict, tseslint.configs.stylistic, {
+    // files: ['src/**/*.ts', 'src/**/*.tsx'],
+    languageOptions: {
+        parserOptions: {
+            project: true,
+            tsconfigRootDir: import.meta.dirname,
+        },
+    },
+    rules: {
+        '@typescript-eslint/no-floating-promises': 'error',
+    },
+});
